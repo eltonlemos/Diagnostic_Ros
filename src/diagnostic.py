104a9e5f7d7125cd3c49c6906d72f260dc9cee52
Elton Lemos <eltonlemos2411@gmail.com>
2024-07-12 17:08:21 -0400

    changed to diagnostics

--- src/diagnostic.py
+++ src/diagnostic.py
@@ -19,7 +19,7 @@ class DiagnosticTalker(Node):
         super().__init__('diagnostic_talker')
         self.i = 0
         self.pub = self.create_publisher(DiagnosticArray,
-                                         '/diagnostic_moog',
+                                         '/diagnostics',
                                          qos_profile_system_default)
         timer_period = 1.0
         self.tmr = self.create_timer(timer_period, self.timer_callback)
