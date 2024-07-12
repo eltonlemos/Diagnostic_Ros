72c3e0cca9d104050c820baa47c623def92bf6d6
Elton Lemos <eltonlemos2411@gmail.com>
2024-07-11 21:20:41 -0400

    params file adjust

--- src/diagnostics.launch.py
+++ src/diagnostics.launch.py
@@ -3,7 +3,7 @@
 import launch
 import launch_ros.actions
 
-analyzer_params_filepath = "analyzers.yaml"
+analyzer_params_filepath = "@ANALYZER_PARAMS_FILEPATH@"
 # add_analyzer_params_filepath = "@ADD_ANALYZER_PARAMS_FILEPATH@"
 
 
